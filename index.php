39de95fbb6c673689e2dd0c35e2a45e4454ce857
Masterhood <120566117+Masterhood@users.noreply.github.com>
2023-04-19 19:26:30 +0200

    Update and rename index.html to index.php
    PHP connection has been added.

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname "issue_manager";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8;", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> <link rel="stylesheet" href="Issue.css"> <meta name="viewport" content="width=device-width, initial-scale=1"><title>Hood IT Support</title></head>
@@ -6,7 +22,7 @@
 <body>
   <nav class="navbar">
     <div class="logo">
-     <a href=index.html ><img src="Hood_logo.jpg" alt="Hood" height= 83.3px ></a>
+     <a href=issue_manager.html ><img src="Hood.jpg" alt="Hood"></a>
       <span>Hood Inc's Issue manager</span>
        </div>
       <a href=Help.html ><button class="help-btn">Help</button></a>
@@ -14,20 +30,20 @@
   </nav>
   <div class="form">
     <form method="POST" action= "<?php echo $_SERVER['PHP_SELF'];?>">
-  <label for="jmeno"></label><br><input type="text" id="jmeno" name="jmeno" placeholder="Name..."><br>
+  <label for="jmeno">Name of reporter:</label><br><input type="text" id="jmeno" name="jmeno"><br>
 <!---
 vypnuto kvůli idee s kradením dat přímo z Pc
     <label for="datum">Date:</label><br><input type="text" id="datum" name="datum"><br>
    <label for="lokace">Localization:</label><br><input type="text" id="lokace" name="lokace"><br>
   </--->
-    <label for="problem"></label><br><input type="text" id="problem" name="problem" placeholder="Type of issue (HW or SW)..."><br>
-  <label for="popis"></label><br><textarea type="text" id="freeform" name="freeform"placeholder="Description..." rows="4" cols="50"></textarea></br>
+    <label for="problem">Type of issue:</label><br><input type="text" id="problem" name="problem"><br>
+  <label for="popis">Description:</label><br><textarea id="freeform" name="freeform" rows="4" cols="50"></textarea></br>
   <input type="submit" value="Submit">
   </div>
   </form>
   <footer>
   <div class="copy">
-  <b>Copyright by Hood's Corporation. For internal use only, do not share. Sharing is forbidden.</b>
+  <b>Copyright by Hood's Corporation.                            For internal use only, do not share. Sharing is forbidden.</b>
   </div>
 
     </footer>
