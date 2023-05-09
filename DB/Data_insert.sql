8e916075cefbc8bb0f31a1de93b4a4f9afcdfda9
Masterhood <120566117+Masterhood@users.noreply.github.com>
2023-05-09 21:43:25 +0200

    New SQL DUMP and related files added

---
+++ DB/Data_insert.sql
@@ -0,0 +1,15 @@
+show tables;
+
+
+select * from issue;
+
+Insert into admin values
+('1','Jana','Dostalova','Heslo0001','jdostalova'),
+('2','Dana','Dlouha','Heslo0002','ddlouha'),
+('3','Magda','Kratka','Heslo0003','mkratka'),
+('4','Edudant','Rygydny','Heslo0004','erygydny'),
+('5','Francimor','Otta','Heslo0005','fotta'),
+('6','Vitezslav','Vzdaleny','Heslo0006','vvzdaleny')
+;
+
+
