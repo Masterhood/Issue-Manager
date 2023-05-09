0880f0c9d0957e89f137d9b5a1dcb8f30b07cbf3
Masterhood <120566117+Masterhood@users.noreply.github.com>
2023-05-09 21:31:20 +0200

    Add files via upload
    .php files added and changed database connection file

---
+++ Adminlog.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname ="issue-manager";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8;", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
+
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> <link rel="stylesheet" href="Issue.css"> <meta name="viewport" content="width=device-width, initial-scale=1"><title>Hood IT Support</title></head>
+</head>
+<body>
+  <nav class="navbar">
+    <div class="logo">
+     <a href=index.php ><img src="Hood_logo.jpg" alt="Hood" height= 83.3px ></a>
+      <span>Hood Inc's Issue manager</span>
+    </div>
+
+  </nav>
+    <div class="form">
+    <form method="POST" action= "<?php echo $_SERVER['PHP_SELF'];?>">
+  <label for="username"></label><br><input type="text" id="username" name="username"placeholder="Name.."><br>
+ <label for="problem"></label><br><input type="password" id="password" name="password"placeholder="Password..."><br>
+  <input type="submit" value="Submit">
+  </div>
+  </form>
+  <!-- add your content here -->
+</body>
+<footer>
+<div class="footer">
+<div class="row">
+<ul>
+<li>Contact  <a href="mailto:issueteam@hoodinc.com"></a></li>
+<li><a href="#">Help</a></li>
+</ul>
+</div>
+<div class="row">
+Hood Incorporation (325 North St. Paul Street #2020 Dallas, TX 75201, USA) Copyright © 2022 Hood - All rights reserved 
+</div>
+</div>
+    </footer>
+<?php
+            } else {       
+            
+                $username = $_POST['username'];
+                $paswword = $_POST['password'];  
+                $hash = md5($id.date('YmdMS'));         
+                              
+                    $sql = "insert into issue (username,paswword)
+                            values ('".$username."','".$paswword."')";
+                    $conn->query($sql);
+                    
+                    echo "<script>alert('Přihlášení do systému je úspěšné);</script>";
+                                                              
+            }
+            ?>
+</html>
---
+++ DB_connect.php
@@ -0,0 +1,16 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname "issue_manager";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8;", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
