9e91c204e9299b74c0d757a5875a0c72564b73a6
Masterhood <120566117+Masterhood@users.noreply.github.com>
2023-05-11 22:05:30 +0200

    Update index.php
    Few PHP commands added

--- index.php
+++ index.php
@@ -29,19 +29,19 @@ catch(PDOException $e)
       <a href=Help.php ><button class="help-btn">Help</button></a>
      <a href=Adminlog.php ><button class="login-btn">Login</button></a>
   </nav>
+  <?php
+  if(!isset($_POST['submit'])){
+  ?>
   <div class="form">
-    <form method="POST" action= "<?php echo $_SERVER['PHP_SELF'];?>">
+
 	
             <form style="text-align:center;" method="post" action="<?php $_SERVER['PHP_SELF'];?>" >
                 <p>
-             <input type="text" name="name"  placeholder="username of reporter..." autofocus />                         
+             <input type="text" name="username"  placeholder="username of reporter..." autofocus />
                 </p>
                  <input type="number" name="id"  placeholder="id of reporter..." autofocus />    
                 <p>
                <input type="text" name="type" placeholder="Type of issue(SW or HW)..." />                 
-                </p> 
-                <p>
-              <input type="number" name="pc_number" placeholder="Number of PC station..."  />                 
                 </p>
                 <!---
 vypnuto kvůli idee s kradením dat přímo z Pc
@@ -49,7 +49,7 @@ vypnuto kvůli idee s kradením dat přímo z Pc
 
   </--->
                 <p>
-                 <textarea id="freeform" name="description" placeholder="Description..." rows="4" cols="50"></textarea>                      
+                 <textarea id="freeform" name="Description" placeholder="Description..." rows="4" cols="50"></textarea>
                 </p>
                 <p>
                 <input type="submit" name="submit" value="Submit">
@@ -76,25 +76,22 @@ Hood Incorporation (325 North St. Paul Street #2020 Dallas, TX 75201, USA) Copyr
 
         
       		<?php
-             
-            
+            } else {
                 $id = $_POST['id'];
                 $username = $_POST['username'];
                 $type = $_POST['type'];
-                $pc_number=$_POST['pc_number']
                 $description = $_POST['Description'];
-                
-                $hash = md5($id.date('YmdMS'));
-                
-                
-                               
-                              
-                    $sql = "insert into issue (id,type,description,pc_number)
-                            values ('".$id."',".$username",'".$type."','".$description"','".$pc_number"'))";
-                    $conn->query($sql);
-                    
-                    echo "<script>alert('Zadání problému je úspěšné);</script>";
-                                                              
+                $hash = md5($id . date('YmdMS'));
+
+
+                $sql = "insert into issue (id,username,type,popis)
+                            values ('".$id."','".$username."','".$type."','".$description."')";
+                $conn->query($sql);
+
+                echo "<script>alert('Zadání problému je úspěšné);</script>";
+
+            }
+                                                        
             
             ?>
 		
